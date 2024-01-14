9d9138a4d3bec2c779ea437ac9595a95b30af81f
Backback12 <cepagtakhan@gmail.com>
2024-01-13 23:00:37 -0600

    Added alerts, Changed to Bootstrap 5.3.2

--- public/client.js
+++ public/client.js
@@ -27,17 +27,20 @@ buttonCreate.addEventListener('click', function(e) {
             if(response.status == 201)
             {
                 output.value = `'${username.value}' was created!`
+                showAlert(0, `${username.value} was created!`)
                 updateDB()
                 return;
             }
             else if(response.status == 204)
             {
                 output.value = `'${username.value}' already exists. Nothing was added.`
+                showAlert(1, `${username.value} already exists.`)
                 return;
             }
             else
             {
                 output.value = `'ERROR: ${response.status}'`
+                showAlert(2, `'ERROR: ${response.status}'`)
                 return;
             }
             
@@ -71,11 +74,13 @@ buttonRead.addEventListener('click', function(e) {
                 // found
                 // console.log("found in database!");
                 output.value = `'${username.value}' was found!`;
+                showAlert(0, `${username.value} was found!`)
             }
             else if (response.status == 204) {
                 // not found
                 // console.log("not found in database");
                 output.value = `'${username.value}' was NOT found.`;
+                showAlert(1, `${username.value} not found`)
             }
 
             updateDB()
@@ -107,12 +112,14 @@ buttonDelete.addEventListener('click', function(e) {
             if(response.status == 200)
             {
                 output.value = `'${username.value}' was deleted.`
+                showAlert(0, `'${username.value}' was deleted.`)
                 updateDB()
                 return;
             }
             else if(response.status == 204)
             {
                 output.value = `'${username.value}' does not exist. No user deleted.`
+                showAlert(1, `'${username.value}' does not exist. No user deleted.`)
                 return; 
             }
 
@@ -146,6 +153,33 @@ function updateDB() {
     .catch(function(error) {
         console.log(error);
     })
+}
+
+var alertTimer = setTimeout(null, null);
+
+function showAlert(prio, message) {
+    var alertDiv = document.getElementById("alert")
+    // remove alert-success alert-warning alert-danger
+    // 0 - success
+    // 1 - warning
+    // 2 - danger
+    var priority = ['alert-success', 'alert-warning', 'alert-danger'][prio]
+    //alertDiv.classList.remove('alert-success', 'alert-warning',  'alert-danger')
+    hideAlert(false)
+    alertDiv.style.display = "block";
+
+    alertDiv.classList.add(priority)
+    alertDiv.innerText = message
+
+    clearTimeout(alertTimer)
+    alertTimer = setTimeout(hideAlert, 3000)
+}
+function hideAlert(hideDisplay=true) {
+    var alertDiv = document.getElementById("alert")
+    alertDiv.classList.remove('alert-success', 'alert-warning',  'alert-danger')
+    if (hideDisplay) {
+        alertDiv.style.display = "none"
+    }
 }
 
 
